7489e564489f2e0e47ad485ee66f5ab1bd46038d
earneszm <zach.earnest@gmail.com>
2016-06-21 18:35:20 -0700

    adjusting stuff

---
+++ MattAndRebeccaWedding/MattAndRebeccaWedding/Controllers/GrubarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MattAndRebeccaWedding.Controllers
+{
+    public class GrubarController : Controller
+    {
+        // GET: Grubar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
